b05a6c657a68918c21b4d6b5f1ccd67f8c9628a5
Krupass <jindrasima11@seznam.cz>
2023-12-11 11:30:09 +0100

    Day04 Part Two - Completed

--- Day04/main.py
+++ Day04/main.py
@@ -8,20 +8,32 @@ if __name__ == '__main__':
             card = card.split(' | ')
             card[0] = card[0].split()
             card[1] = card[1].split()
+            card.append(1)
             cards.append(card)
 
-    points = 0
+    scratchcards = 0
 
-    for card in cards:
-        worth = 0.5
+    for i, card in enumerate(cards):
+        copies = 0
         win = False
         for your_number in card[1]:
             for win_number in card[0]:
                 if your_number == win_number:
                     win = True
-                    worth *= 2
+                    copies += 1
                     break
         if(win):
-            points += worth
+            temp = 1
+            while(temp <= cards[i][2]):
+                tmp = 1
+                while(tmp <= copies):
+                    cards[(i + tmp)][2] += 1
+                    tmp += 1
+                temp += 1
 
-    print(points)
+        print("Card " + str(i+1) + ": " + str(card) + ", win numbers: " + str(copies))
+
+    for card in cards:
+        scratchcards += card[2]
+
+    print(scratchcards)
